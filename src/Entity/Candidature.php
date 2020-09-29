b832dbd05f6d1b8f99c4429335e7f3ddf6d84ffd
aurele <aurelie.nejmah@gmail.com>
2020-09-29 15:30:43 +0200

    Ajout de l'entité Comment

--- src/Entity/Candidature.php
+++ src/Entity/Candidature.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\CandidatureRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -52,6 +54,16 @@ class Candidature
      * @ORM\JoinColumn(nullable=false)
      */
     private $classroom;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Comment::class, mappedBy="candidature", orphanRemoval=true)
+     */
+    private $comments;
+
+    public function __construct()
+    {
+        $this->comments = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -138,6 +150,37 @@ class Candidature
     public function setClassroom(?Classroom $classroom): self
     {
         $this->classroom = $classroom;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Comment[]
+     */
+    public function getComments(): Collection
+    {
+        return $this->comments;
+    }
+
+    public function addComment(Comment $comment): self
+    {
+        if (!$this->comments->contains($comment)) {
+            $this->comments[] = $comment;
+            $comment->setCandidature($this);
+        }
+
+        return $this;
+    }
+
+    public function removeComment(Comment $comment): self
+    {
+        if ($this->comments->contains($comment)) {
+            $this->comments->removeElement($comment);
+            // set the owning side to null (unless already changed)
+            if ($comment->getCandidature() === $this) {
+                $comment->setCandidature(null);
+            }
+        }
 
         return $this;
     }
