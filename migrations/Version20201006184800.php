36b4eb62802a8c052edb3c6df903279bd254f650
aurele <aurelie.nejmah@gmail.com>
2020-10-06 21:02:55 +0200

    Préparation de la mise en ligne

--- migrations/Version20201006184800.php
+++ migrations/Version20201006184800.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20201006120554 extends AbstractMigration
+final class Version20201006184800 extends AbstractMigration
 {
     public function getDescription() : string
     {
